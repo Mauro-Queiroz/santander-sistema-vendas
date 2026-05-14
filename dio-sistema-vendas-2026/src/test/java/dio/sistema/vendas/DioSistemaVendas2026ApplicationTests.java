409297b2a6ae3be8b5a02b25bcf3586d0f94117f
maurolqz@gmail.com <maurolqz@gmail.com>
2026-05-14 14:38:15 -0300

    feat: update projeto dio sistema vendas de Produto.

---
+++ dio-sistema-vendas-2026/src/test/java/dio/sistema/vendas/DioSistemaVendas2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.sistema.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSistemaVendas2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
